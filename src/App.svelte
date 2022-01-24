18c7049d87233afef6a47805c6fc9652c52fb1c1
Furkan Demir <batuhan@extrawatts.co>
2022-01-24 03:43:26 +0300

    :tada: initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<main>
+  <h1>Rinzler</h1>
+  <span>an ordinary music player app</span>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  :global(body) {
+    background-color: #000;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 5rem;
+    color: #fff;
+    margin: 0;
+  }
+
+  span {
+    color: lightblue;
+    font-size: small;
+  }
+</style>
